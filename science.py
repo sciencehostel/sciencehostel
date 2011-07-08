f7a64896b66dc79add56f120d02a710220b376b0
anusheel <anusheel@sciencehostel.org>
2011-07-08 07:24:01 +0000

    Added v1 UI code from @Prajakta

---
+++ science.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os.path
+import re
+import tornado.auth
+import tornado.database
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import unicodedata
+
+from tornado.options import define, options
+
+define("host", default='10.112.61.66', help="run on the given host")
+define("port", default=9090, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1:3306", help="database host")
+define("mysql_database", default="science", help="database name")
+define("mysql_user", default="zoodle", help="database user")
+define("mysql_password", default="zoodle123", help="database password")
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=False,
+            cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        # Have one global connection to the blog DB across all handlers
+        self.db = tornado.database.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
+
+class MainHandler(tornado.web.RequestHandler):
+
+  def get(self):
+    self.write('yo')
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port, options.host)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
